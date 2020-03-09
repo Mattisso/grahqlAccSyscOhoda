9612c28254f8c7bde75375869bfe6b6f8bfb850f
Mensah Attisso <mattisso@atd-us.com>
2020-03-09 07:43:58 -0400

    init-W

--- omodels/ocompte.js
+++ omodels/ocompte.js
@@ -5,80 +5,69 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-
 const oCompteSchema = new Schema({
-  CompteNumber:
-    {
-      type: String
-    },
-    oreferenceID:
-    {
-      type: String
-    },
-   CreatedOn:
-    {
-      type: Date,
-      default:
-        Date.now
-    },
-  CreatedBy:
-    {
-      type: String
-    },
-  ModifiedOn:
-    {
-      type: Date,
-      default:
-        Date.now
-    },
-  ModifiedBy:
-    {
-      type: String
-    },
-  isActive:
-    {
-      type: Boolean,
-      default:
-        true
-    }
+  CompteNumber: {
+    type: String
+  },
+  oreferenceID: {
+    type: String
+  },
+  CreatedOn: {
+    type: Date,
+  default:
+    Date.now
+  },
+  CreatedBy: {
+    type: String
+  },
+  ModifiedOn: {
+    type: Date,
+  default:
+    Date.now
+  },
+  ModifiedBy: {
+    type: String
+  },
+  isActive: {
+    type: Boolean,
+  default:
+    true
+  }
 }, {
-    toObject: {
-      virtuals: true
-    },
-    toJSON: {
-      virtuals: true
-    }
+  toObject: {
+    virtuals: true
+  },
+  toJSON: {
+    virtuals: true
   }
-);
-oCompteSchema.set('toObject', { getters: true });
-oCompteSchema.set('toJSON', { getters: true });
+});
+oCompteSchema.set('toObject', {
+  getters: true
+});
+oCompteSchema.set('toJSON', {
+  getters: true
+});
 
-oCompteSchema.index(
-  {
-    CompteNumber: 1
- //  , Exception: 1
-  }
-);
-
+oCompteSchema.index({
+  CompteNumber: 1
+  //  , Exception: 1
+});
 
 oCompteSchema.pre('save',
   function (next) {
 
+  let currentDate = new Date();
 
-    let currentDate = new Date();
-
-    if (!this.CreatedOn)
-      this.CreatedOn = currentDate;
-    if (!this.ModifiedOn)
-      this.ModifiedOn = currentDate;
-    if (!this.CreatedBy)
-      this.CreatedBy = 'Admin';
-    if (!this.ModifiedBy)
-      this.ModifiedBy = 'Admin';
-    next();
-  }
-);
+  if (!this.CreatedOn)
+    this.CreatedOn = currentDate;
+  if (!this.ModifiedOn)
+    this.ModifiedOn = currentDate;
+  if (!this.CreatedBy)
+    this.CreatedBy = 'Admin';
+  if (!this.ModifiedBy)
+    this.ModifiedBy = 'Admin';
+  next();
+});
 
 const oCompte = mongoose.model('oCompte', oCompteSchema);
 
